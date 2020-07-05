21ed0aede5801e52269991395c7a2752bf16e133
Stuart Izon <stuart.izon@gmail.com>
2020-07-06 01:02:16 +1000

    Implement buildWord function

--- src/main/scala/com/stuartizon/anagrabble/service/WordBuildingService.scala
+++ src/main/scala/com/stuartizon/anagrabble/service/WordBuildingService.scala
@@ -7,5 +7,27 @@ class WordBuildingService {
     *
     * Returns a [[scala.Some]] containing the new game state if the word can be built, or a [[scala.None]] if it is not
     * possible to build this word from the current game state. */
-  def buildWord(game: Game, newWord: Word): Option[Game] = ???
+  def buildWord(game: Game, newWord: Word): Option[Game] = {
+    def extraLettersRequiredForNewWord(from: Word) = newWord.value.diff(from.value).toList
+
+    def allLettersUsedInNewWord(from: Word) = from.value.diff(newWord.value).isEmpty
+
+    def letterPileContains(letters: List[Char]): Boolean = letters.diff(game.letters).isEmpty
+
+
+    game.words.filter(allLettersUsedInNewWord)
+      .map(word => word -> extraLettersRequiredForNewWord(word))
+      .find {
+        case (_, extraLetters) => extraLetters.nonEmpty && letterPileContains(extraLetters)
+      }
+      .map {
+        case (word, extraLetters) => Game(game.players, newWord :: game.words.filterNot(_ == word), game.letters.diff(extraLetters))
+      }
+      .orElse {
+        val letters = newWord.value.toList
+        if (letterPileContains(letters))
+          Some(Game(game.players, newWord :: game.words, game.letters.diff(letters)))
+        else None
+      }
+  }
 }
