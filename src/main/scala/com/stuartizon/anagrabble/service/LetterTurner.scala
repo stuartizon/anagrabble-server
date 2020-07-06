aaad5fef0193a840f255fb96d780aca71e06aaff
Stuart Izon <stuart.izon@gmail.com>
2020-07-06 15:29:50 +1000

    Document turnLetter method

--- src/main/scala/com/stuartizon/anagrabble/service/LetterTurner.scala
+++ src/main/scala/com/stuartizon/anagrabble/service/LetterTurner.scala
@@ -3,6 +3,10 @@ package com.stuartizon.anagrabble.service
 import com.stuartizon.anagrabble.entity.Game
 
 trait LetterTurner {
+  /** Attempt to turn over a letter from the letter bag.
+    *
+    * Returns a [[scala.Some]] containing the new game state if a letter can be turned, or a [[scala.None]] if there
+    * are no more tiles to turn. */
   def turnLetter(game: Game): Option[Game] = {
     if (game.letterBag.letters.nonEmpty) {
       val (newLetter, newLetterBag) = game.letterBag.takeRandomLetter
