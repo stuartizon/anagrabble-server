50e313df63da4aed0ed007e4a77b431f0d7262a0
Stuart Izon <stuart.izon@gmail.com>
2020-07-13 22:18:33 +1000

    Add ECS service deployment to terraform config

--- infra/main.tf
+++ infra/main.tf
@@ -25,3 +25,42 @@ resource "aws_ecs_task_definition" "anagrabble-server" {
     application_version = var.application_version
   })
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_lb" "anagrabble-server" {
+  name = "anagrabble-server"
+  load_balancer_type = "application"
+}
+
+resource "aws_lb_target_group" "anagrabble-server" {
+  name = "anagrabble-server"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_lb_listener" "anagrabble-server" {
+  load_balancer_arn = aws_lb.anagrabble-server.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.anagrabble-server.arn
+  }
+}
+
+resource "aws_ecs_service" "anagrabble-server" {
+  name = "anagrabble-server"
+  task_definition = aws_ecs_task_definition.anagrabble-server.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.anagrabble-server.arn
+    container_name = "anagrabble-server"
+    container_port = 8080
+  }
+}
