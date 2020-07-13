6214b848c2f9529201422917bc4face3a33e29b5
Stuart Izon <stuart.izon@gmail.com>
2020-07-13 22:38:01 +1000

    Set subnets and cluster name for ECS task

--- infra/main.tf
+++ infra/main.tf
@@ -30,9 +30,18 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_ecs_cluster" "default" {
+  cluster_name = "default"
+}
+
 resource "aws_lb" "anagrabble-server" {
   name = "anagrabble-server"
   load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
 }
 
 resource "aws_lb_target_group" "anagrabble-server" {
@@ -54,6 +63,7 @@ resource "aws_lb_listener" "anagrabble-server" {
 
 resource "aws_ecs_service" "anagrabble-server" {
   name = "anagrabble-server"
+  cluster = data.aws_ecs_cluster.default.arn
   task_definition = aws_ecs_task_definition.anagrabble-server.arn
   desired_count = 1
   launch_type = "FARGATE"
