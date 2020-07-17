8a00c2bbb8fdf39b7779d337f8f34bd7eb84b440
Stuart Izon <stuart.izon@gmail.com>
2020-07-17 10:00:50 +1000

    Add logging to websocket messages

--- src/main/scala/com/stuartizon/anagrabble/route/WebSocketRoute.scala
+++ src/main/scala/com/stuartizon/anagrabble/route/WebSocketRoute.scala
@@ -1,9 +1,11 @@
 package com.stuartizon.anagrabble.route
 
 import akka.NotUsed
+import akka.event.LoggingAdapter
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.stream.Attributes
 import akka.stream.scaladsl.Flow
 import com.stuartizon.anagrabble.entity.{Game, PlayerCommand}
 import io.circe.generic.auto._
@@ -23,9 +25,13 @@ class WebSocketRoute(gameFlow: Flow[PlayerCommand, Game, NotUsed]) {
             // Ignore anything which didn't deserialize correctly
             case Right(command) => command
           }
+          .log("Receive")
           .via(gameFlow)
           .map { gameStateUpdate =>
             TextMessage.Strict(gameStateUpdate.asJson.toString)
+          }
+          .addAttributes {
+            Attributes.logLevels(onElement = Attributes.LogLevels.Info)
           }
       }
     }
